24b056d2652caf83e8dc09612bcc50738dee3e9a
jonnyc987 <38059850+jonnyc987@users.noreply.github.com>
2018-12-13 15:49:19 -0800

    CSS Sunset Animation first commit

---
+++ App.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <div className="firstLevel">
+        <div className="sun"></div>
+        <div className="wave-1 waves"></div>
+        <div className="wave-2 waves"></div>
+        <div className="wave-3 waves"></div>
+        <div className="wave-4 waves"></div>
+        <div className="wave-5 waves"></div>
+        <div className="wave-6 waves"></div>
+        <div className="wave-7 waves"></div>
+        <div className="wave-8 waves"></div>
+        <div className="wave-9 waves"></div>
+        <div className="wave-10 waves"></div>
+        <div className="wave-11 waves"></div>
+        <div className="wave-12 waves"></div>
+        <div className="wave-13 waves"></div>
+        <div className="wave-14 waves"></div>
+        <div className="wave-15 waves"></div>
+        <div className="wave-16 waves"></div>
+        <div className="wave-17 waves"></div>
+        <div className="wave-18 waves"></div>
+        <div className="wave-19 waves"></div>
+        <div className="wave-20 waves"></div>
+        <div className="wave-21 waves"></div>
+        <div className="wave-22 waves"></div>
+        <div className="wave-23 waves"></div>
+        <div className="wave-24 waves"></div>
+        <div className="wave-25 waves"></div>
+        <div className="wave-26 waves"></div>
+        <div className="wave-27 waves"></div>
+        <div className="wave-28 waves"></div>
+        <div className="wave-29 waves"></div>
+        <div className="wave-30 waves"></div>
+        <div className="wave-31 waves"></div>
+        <div className="wave-32 waves"></div>
+        <div className="wave-33 waves"></div>
+        <div className="wave-34 waves"></div>
+        <div className="wave-35 waves"></div>
+        <div className="wave-36 waves"></div>
+        <div className="wave-37 waves"></div>
+        <div className="wave-38 waves"></div>
+        <div className="wave-39 waves"></div>
+        <div className="wave-40 waves"></div>
+        <div className="wave-41 waves"></div>
+        <div className="wave-42 waves"></div>
+        <div className="wave-43 waves"></div>
+        <div className="wave-44 waves"></div>
+        <div className="wave-45 waves"></div>
+        <div className="wave-46 waves"></div>
+        <div className="wave-47 waves"></div>
+        <div className="wave-48 waves"></div>
+        <div className="water"></div>
+        <div className="starDiv">
+          <div className="star-1 stars"></div>
+          <div className="star-2 stars"></div>
+          <div className="star-3 stars"></div>
+          <div className="star-4 stars"></div>
+          <div className="star-5 stars"></div>
+          <div className="star-6 stars"></div>
+          <div className="star-7 stars"></div>
+          <div className="star-8 stars"></div>
+          <div className="star-9 stars"></div>
+          <div className="star-10 stars"></div>
+          <div className="moon1"></div>
+        </div>
+      </div>
+      <div className="secondLevel">
+      <div className="island2"></div>
+      </div>
+      <h2 style={{color: "white"}}>To Be Continued</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
